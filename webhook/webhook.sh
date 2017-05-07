e675ff977d54579ea028d04f117c1a53f6bfed9a
zhaif <zhaif@zhihu.com>
2017-05-07 14:28:14 +0800

    bug fix for webhook shell

--- webhook/webhook.sh
+++ webhook/webhook.sh
@@ -3,10 +3,10 @@
 cd ~/hexo_blog_static
 git pull https://github.com/zhai3516/hexo_blog_static.git
 rm -rf ~/hexo_blog/source/_posts/*
-cp ~/hexo_blog_static/blog-md/* ~/hexo_blog/source/_posts/
+cp -r ~/hexo_blog_static/blog-md/* ~/hexo_blog/source/_posts/
 
 cd ~/hexo_blog
 hexo clean
 hexo generate
 rm -rf /var/www/hexo/*
-cp ~/hexo_blog/public/* /var/www/hexo/
+cp -r ~/hexo_blog/public/* /var/www/hexo/
